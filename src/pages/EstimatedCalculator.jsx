6f99c57495e6ef045450a7118de8b14e1e2b093d
Ujjawal <ambikastore.uk@gmail.com>
2024-04-06 11:24:19 +0530

    Update

---
+++ src/pages/EstimatedCalculator.jsx
@@ -0,0 +1,9 @@
+function EstimatedCalculator() {
+    return (
+        <div>
+            Estimated caLculator page
+        </div>
+    )
+}
+
+export default EstimatedCalculator
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="flex">
+      <div>shabad</div>
+      <div>
+        <img src="/hero.jpg" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
