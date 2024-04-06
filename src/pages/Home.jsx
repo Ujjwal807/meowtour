93984d453a6e836c5353fcd27c0e16a0d6659b51
Ujjawal <ambikastore.uk@gmail.com>
2024-04-06 11:49:24 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 function Home() {
   return (
-    <div className="flex">
+    <div className="flex justify-between items-center">
       <div>shabad</div>
       <div>
-        <img src="/hero.jpg" alt="" />
+        <img
+          src="/hero.jpg"
+          alt="hero-image"
+          style={{
+            WebkitMaskImage: "url(/splash.png)",
+            maskImage: "url(/splash.png)",
+            WebkitMaskRepeat: "no-repeat",
+            maskRepeat: "no-repeat",
+            maskSize: "contain",
+          }}
+        />
       </div>
     </div>
   );
